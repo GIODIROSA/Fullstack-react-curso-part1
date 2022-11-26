e3e2a33d8fd40c3a50d41d2bdf90c21a9bb1c025
Giovanni Di Rosa <giodirosaartes@gmail.com>
2022-11-26 13:49:42 -0300

    Mantiene un error, cuando se Json parse undefined. en componente todolist

--- src/component/TodoList.jsx
+++ src/component/TodoList.jsx
@@ -1,9 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Formulario2 from "./Formulario2";
 import Todo from "./Todo";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
+
+  // AVERIGUAR
+  // ====================
+
+  // const [todos, setTodos] = useState(() => {
+
+  //   const enMemoria = localStorage.getItem('todos')
+
+  //   const initialValue = JSON.parse(enMemoria)
+
+  //   return initialValue || ''
+
+  // })
+
+  // ==========================
+
+  //   useEffect(() => {
+  //     console.log("Leer todos local");
+  //     if (localStorage.getItem("todos")) {
+  //         setTodos(JSON.parse(localStorage.getItem("todos")));
+  //     }
+  // }, []);
+
+  useEffect(() => {
+    console.log("Guardar todo local");
+    localStorage.setItem("todos", JSON.stringify());
+  }, [todos]);
+
+  // ==========================
 
   const agregarTodo = (todo) => {
     setTodos((old) => [...old, todo]);
